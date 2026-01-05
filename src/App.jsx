755df29ec1821a8b6c5f775142e0545b280c93f1
Hua <hua@MacBookAirHua.local>
2026-01-05 14:12:29 +0700

    feat : implement counter state and github api fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import { useState, useEffect } from 'react'
 import ProfileCard from "./components/ProfileCard"
 function App(){
+  const [githubData, setGithubData] = useState(null)
+    const username = "HuaTanakrit"
+
+    useEffect(() => {
+        fetch(`https://api.github.com/users/${username}`)
+        .then(res => res.json())
+        .then(data => {
+            setGithubData(data);
+        })
+        .catch(err => console.error(err));
+    }, []);
   return(
     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
       <h1>My Team Portfolio</h1>
@@ -15,6 +27,15 @@ function App(){
         role = "Guest Developer"
         bio = "I love coding and learning new things"
       />
+      {githubData ? (
+        <ProfileCard
+        name = {githubData.name || githubData.login}
+        role = "Guest User"
+        bio = {githubData.bio || "No bio available"}
+        />
+      ) : (
+        <p>Loading data from GitHub...</p>
+      )}
     </div>
   );
 }
