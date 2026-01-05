7179bed860c679359d871e4f1178d00d723d2c43
Hua <hua@MacBookAirHua.local>
2026-01-05 14:14:01 +0700

    chore: remove outline card

--- src/App.jsx
+++ src/App.jsx
@@ -15,18 +15,6 @@ function App(){
   return(
     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
       <h1>My Team Portfolio</h1>
-
-      <ProfileCard
-        name = "ธนกฤต พิทักษ์โกศล"
-        role = "Student @ CEDT"
-        bio = "skibidi roblox"
-      />
-
-      <ProfileCard
-        name = "John Doe"
-        role = "Guest Developer"
-        bio = "I love coding and learning new things"
-      />
       {githubData ? (
         <ProfileCard
         name = {githubData.name || githubData.login}
